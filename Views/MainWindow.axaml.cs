adaadaf7994c3698caf7241c73ebbaab3f7bec23
Михаил Рояк <mikeroyak@gmail.com>
2025-02-19 12:31:11 +0700

    примеры reactive

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,12 +1,23 @@
+using System;
 using Avalonia.Controls;
+using DynamicData;
+using DynamicData.Alias;
+using Paint2.ViewModels;
+using System.Collections.ObjectModel;
 
-namespace Paint2.Views
+namespace veceditor
 {
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+   public partial class MainWindow : Window
+   {
+
+      ViewModelBase vm = new ();
+      ReadOnlyObservableCollection<string> figures;
+      public ReadOnlyObservableCollection<string> Figures => Figures;
+      public MainWindow()
+      {
+         InitializeComponent();
+         vm.Figures.Connect().Select(f => f.Name).SortAndBind(out figures);
+         vm.Figures.CountChanged.Subscribe(c => { });
+      }
+   }
 }
