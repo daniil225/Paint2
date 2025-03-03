a705abea8f71ec7ee5433811355b34e03ca283f6
prinida <andreyprinida@gmail.com>
2025-03-03 23:08:03 +0700

    Add header menu buttons

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -52,23 +52,24 @@ namespace Paint2.ViewModels
 
             FiguresInMenu =
             [
-                new FigureMenuItem("/Assets/Figures/rectangle.svg"),
-                new FigureMenuItem("/Assets/Figures/triangle.svg"),
-                new FigureMenuItem("/Assets/Figures/rhombus.svg"),
-                new FigureMenuItem("/Assets/Figures/trapezoid.svg"),
-                new FigureMenuItem("/Assets/Figures/parallelogram.svg"),
-                new FigureMenuItem("/Assets/Figures/pentagon.svg"),
-                new FigureMenuItem("/Assets/Figures/circle.svg"),
-                new FigureMenuItem("/Assets/Figures/oval.svg"),
-                new FigureMenuItem("/Assets/Figures/line.svg"),
-                new FigureMenuItem("/Assets/Figures/bezier-curve.svg")
+                new FigureMenuItem("/Assets/Figures/rectangle.svg", "Rectangle"),
+                new FigureMenuItem("/Assets/Figures/triangle.svg", "Triangle"),
+                new FigureMenuItem("/Assets/Figures/rhombus.svg", "Rhombus"),
+                new FigureMenuItem("/Assets/Figures/trapezoid.svg", "Trapezoid"),
+                new FigureMenuItem("/Assets/Figures/parallelogram.svg", "Parallelogram"),
+                new FigureMenuItem("/Assets/Figures/pentagon.svg", "Pentagon"),
+                new FigureMenuItem("/Assets/Figures/circle.svg", "Circle"),
+                new FigureMenuItem("/Assets/Figures/oval.svg", "Oval"),
+                new FigureMenuItem("/Assets/Figures/line.svg", "Line"),
+                new FigureMenuItem("/Assets/Figures/bezier-curve.svg", "Cubic Bezier curve")
             ];
             SelectedFigureMenuItem = FiguresInMenu.First();
         }
     }
 
-    public class FigureMenuItem(string iconPath)
+    public class FigureMenuItem(string iconPath, string iconName)
     {
         public string IconPath { get; set; } = iconPath;
+        public string IconName { get; set; } = iconName;
     }
 }
