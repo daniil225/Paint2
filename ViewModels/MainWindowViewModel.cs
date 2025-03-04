17c32f7e33946bb5cdba09a380048b542edb88a7
owtf086 <owtf086@gmail.com>
2025-03-04 18:09:13 +0300

    Добавлена панель с сортировкой по группам

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Controls.Models.TreeDataGrid;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using System.Collections.ObjectModel;
 using System.Reactive;
 using System.Threading.Tasks;
 
@@ -14,6 +16,17 @@ namespace Paint2.ViewModels
         [Reactive] public GridLength GroupsColumnWidth { get; set; }
         public ReactiveCommand<Unit, Unit>? HidePropertiesPanelCommand { get; }
         public ReactiveCommand<Unit, Unit>? HideGroupsPanelCommand { get; }
+        
+        private ObservableCollection<GroupItem> _groups =
+        [
+            new("Группа 1") { Children = { new GroupItem("Элемент 1"), new GroupItem("Элемент 2") } },
+
+            new("Группа 2") { Children = { new GroupItem("Элемент 1"), new ("Элемент 2"), new GroupItem("Элемент 3") } }
+        ];
+        public HierarchicalTreeDataGridSource<GroupItem> TreeSource { get; }
+
+        public ReactiveCommand<Unit, Unit> AddElementCommand { get; }
+        
 
         public MainWindowViewModel()
         {
@@ -44,6 +57,36 @@ namespace Paint2.ViewModels
                         : new GridLength(0);
                 });
             });
+
+            TreeSource = new HierarchicalTreeDataGridSource<GroupItem>(_groups)
+            {
+                Columns =
+                {
+                    new HierarchicalExpanderColumn<GroupItem>(
+                        new TextColumn<GroupItem, string>("Название", x => x.Name), x => x.Children)
+                }
+            };
+
+            AddElementCommand = ReactiveCommand.Create(() =>
+            {
+                if (_groups.Count > 0)
+                {
+                    _groups[0].Children.Add(new GroupItem($"Элемент { _groups[0].Children.Count + 1}"));
+                    TreeSource.Items = _groups;
+                }
+            });
+        }
+    }
+    
+    public class GroupItem
+    {
+        public string Name { get; set; }
+        public ObservableCollection<GroupItem> Children { get; }
+
+        public GroupItem(string name)
+        {
+            Name = name;
+            Children = new ObservableCollection<GroupItem>();
         }
     }
 }
