f3f11e051d30da7c2ef7b9c39fbe3c4dca74d679
Vefery <54257165+Vefery@users.noreply.github.com>
2025-03-09 23:25:29 +0700

    Добавил группы и каркас для сцены

---
+++ ViewModels/Group.cs
@@ -0,0 +1,32 @@
+﻿using Interfaces;
+using System.Collections.Generic;
+
+namespace Paint2.ViewModels
+{
+    // При отрисовке по порядку проходятся все уровни иерархии групп, порядок внутри иерархии определяется порядком внутри листов
+    public class Group
+    {
+        // Видимое имы группы
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    _name = value;
+            }
+        }
+        // Тут хранятся все фигуры группы
+        public IList<IFigure> figures;
+        // Тут все дочерние группы
+        public IList<Group> childGroups;
+
+        private string _name;
+        public Group(string name)
+        {
+            _name = name;
+            figures = [];
+            childGroups = [];
+        }
+    }
+}
---
+++ ViewModels/Scene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Paint2.ViewModels
+{
+    public static class Scene
+    {
+        // Тут хранятся все группы сцены. Корневой группы явно нет, по сути сама сцена ей является
+        public static IList<Group> Groups { get; set; }
+        public static IFileStrategy FileStrategy { get; set; }
+
+        static Scene()
+        {
+            Groups = [];
+            FileStrategy = new SVGStrategy();
+        }
+        // Не помню какие именно тут параметры нужны были
+        public static void SaveScene(string path)
+        {
+            FileStrategy.Save(path);
+        }
+        public static void LoadScene(string path)
+        {
+            FileStrategy.Load(path);
+        }
+        public static void ResetScene()
+        {
+            Groups.Clear();
+        }
+    }
+}
