223b49bb75d4c62eaf2fa7457f593cc986660a1e
ITomchakovskiy <pm13.tomchakovskiy@gmail.com>
2025-03-18 06:37:31 +0700

    добавил экспорт в pdf

---
+++ Models/Formats/PDF/PDFExporter.cs
@@ -0,0 +1,158 @@
+﻿using Formats.Svg;
+using iText.Kernel.Pdf.Canvas;
+using iText.Kernel.Pdf;
+using Paint2.ViewModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iText.Kernel.Geom;
+using ExCSS;
+using Avalonia.Controls.Shapes;
+using Paint2.ViewModels.Utils;
+
+namespace Formats.PDF;
+
+public class PDFExporter : IExportFormat
+{
+    public void SaveTo(IExportSnapshot snapshot, string destinationPath)
+    {
+        var pDFSnapshot = snapshot as PDFSnapshot;
+
+        PdfWriter writer = new PdfWriter(destinationPath);
+        PdfDocument pdf = new PdfDocument(writer);
+        
+        // Добавляем страницу
+        PdfPage page = pdf.AddNewPage(new PageSize(pDFSnapshot._width, pDFSnapshot._height));
+
+        // Создаём PdfCanvas для рисования
+        PdfCanvas canvas = new PdfCanvas(page);
+
+        SaveElement(pDFSnapshot._tree,canvas);
+    }
+
+    private void ApplyTransformations(PDFElement pdfElement, PdfCanvas canvas)
+    {
+        if (pdfElement.Transforms == null) return;
+
+        ITransform[] Transforms = pdfElement.Transforms.ToArray();
+
+        foreach ( ITransform transform in Transforms )
+        {
+            switch(transform)
+            {
+                case (Translate shift):
+                    canvas.ConcatMatrix(1, 0, 0, 1, shift.X, shift.Y);
+                    break;
+                case (Rotate rotation):
+                    double rad = Math.PI / 180 * rotation.Angle;
+                    double cos = Math.Cos(rad);
+                    double sin = Math.Sin(rad);
+
+                    if(rotation.Pivot == null)
+                    {
+                        canvas.ConcatMatrix(cos, sin, -sin, cos, 0, 0);
+                        break;
+                    }
+                        
+                    canvas.ConcatMatrix(1, 0, 0, 1, rotation.Pivot.X, rotation.Pivot.Y);
+                    canvas.ConcatMatrix(cos, sin, -sin, cos, 0, 0);
+                    canvas.ConcatMatrix(1, 0, 0, 1, -rotation.Pivot.X, -rotation.Pivot.Y);
+
+                    break;
+            }
+        }
+        //canvas.SaveState();
+        //canvas.ConcatMatrix()
+    }
+
+    private void ApplyBrush(PDFElement pdfElement, PdfCanvas canvas)
+    {
+        if (pdfElement.Brush == null)
+            return;
+        Formats.Brush Brush = pdfElement.Brush;
+        canvas.SetStrokeColorRgb(Brush.Stroke.R, Brush.Stroke.G, Brush.Stroke.B);
+
+        canvas.SetLineWidth((float)Brush.StrokeWidth);
+
+        canvas.SetFillColorRgb(Brush.Fill.R, Brush.Fill.G, Brush.Fill.B);
+
+        if (Brush.Dash == null)
+            return;
+        canvas.SetLineDash((float)Brush.Dash.Value.Length, (float)Brush.Dash.Value.Gap);
+    }
+
+    private void SaveElement(PDFRect rect, PdfCanvas canvas)
+    {
+        canvas.SaveState();
+        ApplyTransformations(rect, canvas);
+        ApplyBrush(rect, canvas);
+        canvas.Rectangle(rect.Position.X, rect.Position.Y, rect.Width, rect.Height);
+        canvas.FillStroke();
+        canvas.RestoreState();
+    }
+
+    private void SaveElement(PDFCircle circle, PdfCanvas canvas)
+    {
+        canvas.SaveState();
+        ApplyTransformations(circle, canvas);
+        ApplyBrush(circle, canvas);
+        canvas.Circle(circle.Center.X, circle.Center.Y, circle.Radius);
+        canvas.FillStroke();
+        canvas.RestoreState();
+    }
+
+    private void SaveElement(PDFLine line, PdfCanvas canvas)
+    {
+        canvas.SaveState();
+        ApplyTransformations(line, canvas);
+        ApplyBrush(line, canvas);
+        canvas.MoveTo(line.Start.X, line.Start.Y);
+        canvas.LineTo(line.End.X, line.End.Y);
+        canvas.Stroke();
+        canvas.RestoreState();
+    }
+
+    private void SaveElement(PDFPolyline polyline, PdfCanvas canvas)
+    {
+        canvas.SaveState();
+        ApplyTransformations(polyline, canvas);
+        ApplyBrush(polyline, canvas);
+        Paint2.ViewModels.Utils.Point[] points = polyline.Points.ToArray();
+        canvas.MoveTo(points[0].X, points[0].Y);
+        for(int i = 1; i < points.Length; i++)
+            canvas.LineTo(points[i].X, points[i].Y);
+        canvas.Stroke();
+        canvas.RestoreState();
+    }
+
+    private void SaveElement(PDFPolygon polygon, PdfCanvas canvas)
+    {
+        canvas.SaveState();
+        ApplyTransformations(polygon, canvas);
+        ApplyBrush(polygon, canvas);
+        Paint2.ViewModels.Utils.Point[] points = polygon.Points.ToArray();
+
+        canvas.MoveTo(points[0].X, points[0].Y);
+
+        for (int i = 1; i < points.Length; i++)
+            canvas.LineTo(points[i].X, points[i].Y);
+
+        canvas.ClosePathFillStroke();
+        canvas.RestoreState();
+    }
+
+    private void SavePath()
+    {
+
+    }
+
+    private void SaveElement(PDFGroup group, PdfCanvas canvas)
+    {
+
+    }
+}
+
+
+
---
+++ Models/Formats/PDF/PDFSnapshot.cs
@@ -0,0 +1,198 @@
+﻿using Avalonia.Media;
+using Formats;
+using Paint2.ViewModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Paint2.ViewModels.Utils;
+using Paint2.Models.Figures;
+using Avalonia.Controls.Shapes;
+
+namespace Formats.PDF;
+
+public class PDFSnapshot : IExportSnapshot
+{
+    public float _width { get; }
+    public float _height { get; }
+    public PDFGroup _tree { get; }
+    PDFGroup _currentGroup;
+
+    public Formats.Brush? Brush { get; set; }
+
+    public PDFSnapshot(float width, float height)
+    {
+        _width = width;
+        _height = height;
+
+        _tree = new PDFGroup();
+        _currentGroup = _tree;
+    }
+
+    public void AppendCircle(DocCircle circle)
+    {
+        PDFCircle pDFCircle = new PDFCircle(circle);
+        _currentGroup.Children.Add(pDFCircle);
+    }
+
+    public void AppendLine(DocLine line)
+    {
+        PDFLine pDFLine = new PDFLine(line);
+        _currentGroup.Children.Add(pDFLine);
+    }
+
+    public void AppendPath(DocPath path)
+    {
+        PDFPath pDPath = new PDFPath(path);
+        _currentGroup.Children.Add(pDPath);
+    }
+
+    public void AppendPolygon(DocPolygon polygon)
+    {
+        PDFPolygon pDPolygon = new PDFPolygon(polygon);
+        _currentGroup.Children.Add(pDPolygon);
+    }
+
+    public void AppendPolyline(DocPolyline polyline)
+    {
+        PDFPolyline pDPolyline = new PDFPolyline(polyline);
+        _currentGroup.Children.Add(pDPolyline);
+    }
+
+    public void AppendRect(DocRect rect)
+    {
+        PDFRect pDRect = new PDFRect(rect);
+        _currentGroup.Children.Add(pDRect);
+    }
+
+    public void Pop()
+    {
+        if(_currentGroup.Parent != null) 
+            _currentGroup = _currentGroup.Parent;
+    }
+
+    public void PushGroup(DocGroup group)
+    {
+        PDFGroup pDFGroup = new PDFGroup(_currentGroup,group);
+
+        _currentGroup.Children.Add(pDFGroup);
+        _currentGroup = pDFGroup;
+    }
+}
+
+public abstract class PDFElement
+{
+    public string? Name { get; set; }
+    public IEnumerable<ITransform>? Transforms { get; set; }
+    public Formats.Brush? Brush { get; set; }
+}
+
+public class PDFGroup : PDFElement
+{
+    public List<PDFElement> Children { get; set; } = new();
+    public PDFGroup? Parent { get; set; }
+
+    public PDFGroup(PDFGroup? parent, DocGroup group)
+    {
+        Parent = parent;
+        Name = group.Name;
+        Children = new();
+        Transforms = group.Transforms;
+        this.Brush = Brush;
+    }
+
+    public PDFGroup() { }
+}
+
+public class PDFRect : PDFElement
+{
+    //public double PositionX { get; set; }
+    public Point Position { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public PDFRect(DocRect rect)
+    {
+        Position = rect.Position;
+        //PositionY = rect.Position.Y;
+        Width = rect.Width;
+        Height = rect.Height;
+        Name = rect.Name;
+        Transforms = rect.Transforms;
+        this.Brush = Brush;
+    }
+}
+
+public class PDFCircle : PDFElement
+{
+    public Point Center { get; set; }
+    //public double CenterY { get; set; }
+    public double Radius { get; set; }
+
+    public PDFCircle(DocCircle circle)
+    {
+        //Center = circle.Center;
+        Center = circle.Center;
+        Radius = circle.Radius;
+        Name = circle.Name;
+        Transforms = circle.Transforms;
+        this.Brush = Brush;
+    }
+}
+
+public class PDFLine : PDFElement
+{
+    public Point Start { get; set; }
+    public Point End { get; set; }
+
+    public PDFLine(DocLine line)
+    {
+        Start = line.Start;
+        End = line.End;
+        Name = line.Name;
+        Transforms = line.Transforms;
+        this.Brush = Brush;
+    }
+
+}
+
+public class PDFPolyline : PDFElement
+{
+    public IEnumerable<Point> Points { get; set; }
+    public PDFPolyline(DocPolyline polyline)
+    {
+        Points = polyline.Points;
+        Name = polyline.Name;
+        Transforms = polyline.Transforms;
+        this.Brush = Brush;
+    }
+}
+
+public class PDFPolygon : PDFElement
+{
+    public IEnumerable<Point> Points { get; set; }
+
+    public PDFPolygon(DocPolygon polygon)
+    {
+        Points = polygon.Points;
+        Name = polygon.Name;
+        Transforms = polygon.Transforms;
+        this.Brush = Brush;
+    }
+}
+
+public class PDFPath : PDFElement
+{
+    public IEnumerable<IPathElement> Elements { get; set; }
+
+    public PDFPath(DocPath path)
+    {
+        Elements = path.Elements;
+        Name = path.Name;
+        Transforms = path.Transforms;
+        this.Brush = Brush;
+    }
+}
